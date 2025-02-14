c038b692e97d9d299b8c4878df8798deb9ebe83b
Sagar Barate <sagarbarate2907@com.com>
2025-02-14 23:44:12 +0000

    Initial commit

---
+++ src/main/java/org/example/accessmodifier2/Main.java
@@ -0,0 +1,15 @@
+package org.example.accessmodifier2;
+
+import org.example.accessmodifiers.Student;
+
+public class Main {
+    public static void main(String[] args) {
+//        Student student = new Student();
+//        student.name = "Ajith";
+        // student.batchId = 10;
+        // student.age = 27;
+        // student.psp = 90.0;
+        System.out.println("Added something for git demo");
+    }
+
+}
